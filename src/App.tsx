9bafefd4d5a5896463e74239ccd4d472f42736e6
Saikumar Perumalla <saikumar.perumalla@bentolabs.io>
2025-09-05 01:34:01 +0530

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
-import LandingPage from './pages/LandingPage';
-import EmployeesPage from './pages/EmployeesPage';
-import TimesheetsPage from './pages/TimesheetsPage';
-import PayrunPage from './pages/PayrunPage';
 import Navbar from './components/Navbar';
+
+// Lazy load the pages
+const LandingPage = lazy(() => import('./pages/LandingPage'));
+const EmployeesPage = lazy(() => import('./pages/EmployeesPage'));
+const TimesheetsPage = lazy(() => import('./pages/TimesheetsPage'));
+const PayrunPage = lazy(() => import('./pages/PayrunPage'));
 
 const AppContent: React.FC = () => {
 
@@ -15,12 +17,14 @@ const AppContent: React.FC = () => {
   return (
     <>
       {showNavbar && <Navbar />}
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/employees" element={<EmployeesPage />} />
-        <Route path="/timesheets" element={<TimesheetsPage />} />
-        <Route path="/payruns" element={<PayrunPage />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/employees" element={<EmployeesPage />} />
+          <Route path="/timesheets" element={<TimesheetsPage />} />
+          <Route path="/payruns" element={<PayrunPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
